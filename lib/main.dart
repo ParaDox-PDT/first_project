94998b778ac3b4e8a93810353f9eb5b59482a320
ParaDox-PDT <doniyorjorabekov@gmail.com>
2023-05-28 22:08:03 +0500

    New scrren

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,214 @@
+import 'package:first_project/utils/app_images.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    return const MaterialApp(
+      home: HomeScreen(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
+      backgroundColor: const Color(0xFFDEDEDE),
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        backgroundColor: const Color(0xFFDEDEDE),
+        actions: [
+          Container(
+            margin: const EdgeInsets.only(
+              right: 24,
+            ),
+            child: Image.asset(
+              AppImages.icon,
+            ),
+          )
+        ],
+        elevation: 0,
+        title: const Text(
+          "Materials",
+          style: TextStyle(
+              fontSize: 24,
+              color: Color(0xFF21242D),
+              fontFamily: "Poppins",
+              fontWeight: FontWeight.w600),
+        ),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+          children: [
+            Image.asset(
+              AppImages.backgroundImg,
+              height: 203,
+              width: 143,
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            const SizedBox(
+              height: 40,
             ),
+            Row(
+              children: [
+                const SizedBox(
+                  width: 43,
+                ),
+                Container(
+                  width: 104,
+                  height: 48,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(25),
+                    color: const Color(0xFFE4E6EA),
+                  ),
+                  child: const Center(
+                    child: Text(
+                      "Document",
+                      style: TextStyle(
+                          fontSize: 14,
+                          color: Color(0xFF21242D),
+                          fontWeight: FontWeight.w500,
+                          fontFamily: "Poppins"),
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  width: 36.5,
+                ),
+                Container(
+                  height: 48,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(25),
+                  ),
+                  child: const Center(
+                    child: Text(
+                      "Exam",
+                      style: TextStyle(
+                          fontSize: 14,
+                          color: Color(0xFF757C8E),
+                          fontWeight: FontWeight.w500,
+                          fontFamily: "Poppins"),
+                    ),
+                  ),
+                ),
+                const SizedBox(width: 60.5),
+                Container(
+                  height: 48,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(25),
+                  ),
+                  child: const Center(
+                    child: Text(
+                      "Passed",
+                      style: TextStyle(
+                          fontSize: 14,
+                          color: Color(0xFF757C8E),
+                          fontWeight: FontWeight.w500,
+                          fontFamily: "Poppins"),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 40,
+            ),
+            Container(
+              width: double.infinity,
+              margin: const EdgeInsets.symmetric(horizontal: 30),
+              child: Row(
+                children: [
+                  getConteiner(
+                      title: "Physics",
+                      color: const Color(0xFFD3D5FE),
+                      icn: "assets/svg/physics.svg"),
+                  const SizedBox(width: 38),
+                  getConteiner(
+                      title: "Science",
+                      color: const Color(0xFFFFEFDA),
+                      icn: "assets/svg/microscope.svg"),
+                  const SizedBox(width: 39),
+                  getConteiner(
+                      title: "Chemistry",
+                      color: const Color(0xFFFFE4F1),
+                      icn: "assets/svg/flask.svg"),
+                  const SizedBox(width: 34),
+                  getConteiner(
+                      title: "Biology",
+                      color: const Color(0xFFCFE5FC),
+                      icn: "assets/svg/dna.svg")
+                ],
+              ),
+            ),
+            const SizedBox(height: 32,),
+            Container(
+              width: double.infinity,
+              margin: const EdgeInsets.symmetric(horizontal: 30),
+              child: Row(
+                children: [
+                  getConteiner(
+                      title: "Math",
+                      color: const Color(0xFFFFCECA),
+                      icn: "assets/svg/calculator.svg"),
+                  const SizedBox(width: 38),
+                  getConteiner(
+                      title: "Science",
+                      color: const Color(0xFFDAFFD6),
+                      icn: "assets/svg/translation.svg"),
+                  const SizedBox(width: 39),
+                  getConteiner(
+                      title: "Literature",
+                      color: const Color(0xFFD5BEFB),
+                      icn: "assets/svg/literature.svg"),
+                ],
+              ),
+            )
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+}
+
+Widget getConteiner(
+    {required String title, required Color color, required icn}) {
+  return Column(
+    children: [
+      Container(
+        width: 54,
+        height: 54,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(16), color: color),
+        child: Center(
+          child: SvgPicture.asset(
+            icn,
+            height: 32,
+            width: 32,
+          ),
+        ),
+      ),
+      const SizedBox(
+        height: 12,
+      ),
+      Text(
+        title,
+        style: const TextStyle(
+            fontFamily: "Poppins",
+            fontSize: 12,
+            fontWeight: FontWeight.w600,
+            color: Color(
+              0xFF21242D,
+            )),
+      )
+    ],
+  );
 }
---
+++ lib/utils/app_images.dart
@@ -0,0 +1,4 @@
+class AppImages{
+  static const String icon = "assets/images/icon.png";
+  static const String backgroundImg = "assets/images/backgroundImg.png";
+}
