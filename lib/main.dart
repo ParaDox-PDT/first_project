eed632d0bbd6f710e5c556ccb64d97b2aaa1fc60
ParaDox-PDT <doniyorjorabekov@gmail.com>
2023-05-28 23:48:54 +0500

    Second screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:first_project/utils/app_images.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
@@ -24,154 +23,131 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color(0xFFDEDEDE),
+      backgroundColor: const Color(0xFFFFFFFF),
       appBar: AppBar(
-        backgroundColor: const Color(0xFFDEDEDE),
-        actions: [
-          Container(
-            margin: const EdgeInsets.only(
-              right: 24,
-            ),
-            child: Image.asset(
-              AppImages.icon,
-            ),
-          )
-        ],
+        backgroundColor: const Color(0xFFFFFFFF),
         elevation: 0,
         title: const Text(
-          "Mat",
+          "Account",
           style: TextStyle(
               fontSize: 24,
+              fontWeight: FontWeight.w600,
               color: Color(0xFF21242D),
-              fontFamily: "Poppins",
-              fontWeight: FontWeight.w600),
+              fontFamily: "Poppins"),
         ),
+        actions: const [
+          Icon(
+            Icons.more_horiz_outlined,
+            color: Color(0xFF21242D),
+            size: 24,
+          ),
+          SizedBox(
+            width: 28,
+          )
+        ],
       ),
       body: Center(
         child: Column(
           children: [
+            const SizedBox(
+              height: 8,
+            ),
             Image.asset(
-              AppImages.backgroundImg,
-              height: 203,
-              width: 143,
+              "assets/images/mainImg.png",
             ),
             const SizedBox(
-              height: 40,
+              height: 16,
+            ),
+            const Text(
+              "Ayano Nana",
+              style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.w600,
+                  color: Color(0xFF21242D),
+                  fontFamily: "Poppins"),
+            ),
+            const SizedBox(
+              height: 8,
+            ),
+            const Text(
+              "Hello every body, I will score high in",
+              style: TextStyle(
+                  fontFamily: "Poppins",
+                  fontSize: 12,
+                  fontWeight: FontWeight.w400,
+                  color: Color(0xFF757C8E)),
+            ),
+            const SizedBox(
+              height: 6,
+            ),
+            const Text(
+              "this software.",
+              style: TextStyle(
+                  fontFamily: "Poppins",
+                  fontSize: 12,
+                  fontWeight: FontWeight.w400,
+                  color: Color(0xFF757C8E)),
+            ),
+            const SizedBox(
+              height: 28,
             ),
             Row(
               children: [
+                getInfo(
+                    color: const Color(0xFFD3D5FE),
+                    text: "Courses",
+                    n: "14",
+                    img: "assets/svg/document.svg"),
                 const SizedBox(
-                  width: 43,
+                  width: 16,
                 ),
-                Container(
-                  width: 104,
-                  height: 48,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(25),
-                    color: const Color(0xFFE4E6EA),
-                  ),
-                  child: const Center(
-                    child: Text(
-                      "Document",
-                      style: TextStyle(
-                          fontSize: 14,
-                          color: Color(0xFF21242D),
-                          fontWeight: FontWeight.w500,
-                          fontFamily: "Poppins"),
-                    ),
-                  ),
-                ),
+                getInfo(
+                    color: const Color(0xFFDAFFD6),
+                    text: "Score",
+                    n: "82",
+                    img: "assets/svg/scoreboard.svg"),
                 const SizedBox(
-                  width: 36.5,
-                ),
-                Container(
-                  height: 48,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(25),
-                  ),
-                  child: const Center(
-                    child: Text(
-                      "Exam",
-                      style: TextStyle(
-                          fontSize: 14,
-                          color: Color(0xFF757C8E),
-                          fontWeight: FontWeight.w500,
-                          fontFamily: "Poppins"),
-                    ),
-                  ),
-                ),
-                const SizedBox(width: 60.5),
-                Container(
-                  height: 48,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(25),
-                  ),
-                  child: const Center(
-                    child: Text(
-                      "Passed",
-                      style: TextStyle(
-                          fontSize: 14,
-                          color: Color(0xFF757C8E),
-                          fontWeight: FontWeight.w500,
-                          fontFamily: "Poppins"),
-                    ),
-                  ),
+                  width: 23,
                 ),
               ],
             ),
             const SizedBox(
-              height: 40,
+              height: 16,
+            ),
+            Row(
+              children: [
+                getInfo(
+                    color: const Color(0xFFCFE5FC),
+                    text: "Friends",
+                    n: "123",
+                    img: "assets/svg/male.svg"),
+                const SizedBox(
+                  width: 16,
+                ),
+                getInfo(
+                    color: const Color(0xFFFFE4F1),
+                    text: "Following",
+                    n: "89",
+                    img: "assets/svg/femaler.svg"),
+                const SizedBox(
+                  width: 23,
+                ),
+              ],
             ),
             Container(
-              width: double.infinity,
-              margin: const EdgeInsets.symmetric(horizontal: 30),
-              child: Row(
-                children: [
-                  getConteiner(
-                      title: "Physics",
-                      color: const Color(0xFFD3D5FE),
-                      icn: "assets/svg/physics.svg"),
-                  const SizedBox(width: 38),
-                  getConteiner(
-                      title: "Science",
-                      color: const Color(0xFFFFEFDA),
-                      icn: "assets/svg/microscope.svg"),
-                  const SizedBox(width: 39),
-                  getConteiner(
-                      title: "Chemistry",
-                      color: const Color(0xFFFFE4F1),
-                      icn: "assets/svg/flask.svg"),
-                  const SizedBox(width: 34),
-                  getConteiner(
-                      title: "Biology",
-                      color: const Color(0xFFCFE5FC),
-                      icn: "assets/svg/dna.svg")
-                ],
-              ),
-            ),
-            const SizedBox(height: 32,),
-            Container(
-              width: double.infinity,
-              margin: const EdgeInsets.symmetric(horizontal: 30),
-              child: Row(
-                children: [
-                  getConteiner(
-                      title: "Math",
-                      color: const Color(0xFFFFCECA),
-                      icn: "assets/svg/calculator.svg"),
-                  const SizedBox(width: 38),
-                  getConteiner(
-                      title: "Science",
-                      color: const Color(0xFFDAFFD6),
-                      icn: "assets/svg/translation.svg"),
-                  const SizedBox(width: 39),
-                  getConteiner(
-                      title: "Literature",
-                      color: const Color(0xFFD5BEFB),
-                      icn: "assets/svg/literature.svg"),
-                ],
-              ),
+                margin: const EdgeInsets.only(left: 24),
+                child:Column(
+                  children: [
+                    const SizedBox(height: 28,),
+                    getRow(icon: "assets/svg/video.svg", text: "Video Playback"),
+                    const SizedBox(height: 20,),
+                    getRow(icon: "assets/svg/doc.svg", text: "Download Resource"),
+                    const SizedBox(height: 20,),
+                    getRow(icon: "assets/svg/calendar.svg", text: "Schedule"),
+                  ],
+                )
             )
+
           ],
         ),
       ),
@@ -179,35 +155,93 @@ class HomeScreen extends StatelessWidget {
   }
 }
 
-Widget getConteiner(
-    {required String title, required Color color, required icn}) {
-  return Column(
+Widget getInfo(
+    {required Color color,
+    required String text,
+    required String n,
+    required String img}) {
+  return Expanded(
+    child: Container(
+      margin: const EdgeInsets.only(left: 24),
+      width: 165,
+      height: 84,
+      decoration: BoxDecoration(
+          border: Border.all(
+            color: const Color(0xFFE4E6EA),
+            width: 1,
+          ),
+          borderRadius: BorderRadius.circular(20)),
+      child: Row(
+        children: [
+          const SizedBox(
+            width: 20,
+          ),
+          Container(
+            width: 40,
+            height: 40,
+            padding: const EdgeInsets.all(10),
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20), color: color),
+            child: SvgPicture.asset(
+              img,
+            ),
+          ),
+          const SizedBox(
+            width: 12,
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                text,
+                style: const TextStyle(
+                    fontSize: 12,
+                    fontWeight: FontWeight.w400,
+                    fontFamily: "Poppins",
+                    color: Color(0xFF353945)),
+              ),
+              const SizedBox(
+                height: 6,
+              ),
+              Text(
+                n,
+                style: const TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.w600,
+                    fontFamily: "Poppins",
+                    color: Color(0xFF23262F)),
+              )
+            ],
+          )
+        ],
+      ),
+    ),
+  );
+}
+
+Widget getRow({required String icon,required String text}){
+  return Row(
     children: [
       Container(
-        width: 54,
-        height: 54,
+        width: 40,
+        height: 40,
+        padding: const EdgeInsets.all(8),
         decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(16), color: color),
-        child: Center(
-          child: SvgPicture.asset(
-            icn,
-            height: 32,
-            width: 32,
-          ),
-        ),
+            borderRadius: BorderRadius.circular(20),
+            color: const Color(0xFF458CFF)),
+        child: SvgPicture.asset(icon),
       ),
       const SizedBox(
-        height: 12,
+        width: 16,
       ),
       Text(
-        title,
+        text,
         style: const TextStyle(
             fontFamily: "Poppins",
-            fontSize: 12,
-            fontWeight: FontWeight.w600,
-            color: Color(
-              0xFF21242D,
-            )),
+            fontSize: 16,
+            fontWeight: FontWeight.w500,
+            color: Color(0xFF21242D)),
       )
     ],
   );
